b87ddc7f44d917fb771b9a938571b1d9a70a964f
skm5107 <35884686+skm5107@users.noreply.github.com>
2020-07-30 08:35:27 -0400

    minimizing cell nesting in table readin

--- FileHandling/@Formatter/formatType.m
+++ FileHandling/@Formatter/formatType.m
@@ -2,12 +2,16 @@ function out = formatType(self, wasSplit)
     fcnHndl = Tbl.lookup(Frmt.key, self.FormSpec.typeSpec, "typeID", "convertHndl");
     fcnHndl = Arr.uncell(fcnHndl);
     if wasSplit
-        out = cellfun(@(row) byRow(row, fcnHndl, self.FormSpec.styleSpec), self.mid, 'uni', false);
+        out = cellfun(@(row) byRow(row, fcnHndl, self.FormSpec.styleSpec, wasSplit), self.mid, 'uni', false);
     else
-        out = cellfun(@(row) byRow(row, fcnHndl, self.FormSpec.styleSpec), Arr.cellify(self.mid));
+        out = cellfun(@(row) byRow(row, fcnHndl, self.FormSpec.styleSpec, wasSplit), Arr.cellify(self.mid));
     end
 end
 
-function row = byRow(rowRaw, hndl, style)
-    row = cellfun(@(raw) hndl(raw, style), Arr.cellify(rowRaw), 'uni', 0);
+function row = byRow(rowRaw, hndl, style, wasSplit)
+    if wasSplit
+        row = cellfun(@(raw) hndl(raw, style), Arr.cellify(rowRaw), 'uni', 0);
+    else
+        row = cellfun(@(raw) hndl(raw, style), Arr.cellify(rowRaw));
+    end
 end
